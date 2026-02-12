ec1633726307073c52db2243b3d1fbe9193830ef
huiminheng <heng.hm0211@gmail.com>
2026-02-12 17:47:14 +0800

    Add conditional rendering for stocklist

--- stockform-capstone-hhm/src/components/StockList.jsx
+++ stockform-capstone-hhm/src/components/StockList.jsx
@@ -1,6 +1,9 @@
 import StockItem from "./StockItem";
 
 function StockList({ stocks, calculateProfitLoss, getProfitLossColor }) {
+  if (!stocks || stocks.length === 0) {
+    return <div>No stocks available. Please add a stock.</div>;
+  }
   return (
     <ul>
       {stocks.map((stock) => (
